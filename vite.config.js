ffc90ee75e06d57b30b783699d5d1d65502db022
Amirul <amirul.naim@zuscoffee.com>
2023-09-01 09:53:31 +0800

    Update to Laravel 10

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import dotenv from 'dotenv'
+
+dotenv.config() // load env vars from .env
+
+const host = process.env.VITE_APP_URL;
+const port = process.env.VITE_PORT;
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    server: { 
+        host, 
+        hmr: { host }, 
+        port: port, 
+        // https: { 
+        //     key: fs.readFileSync(`/path/to/${host}.key`), 
+        //     cert: fs.readFileSync(`/path/to/${host}.crt`), 
+        // }, 
+    },
 });
