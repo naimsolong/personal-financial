367f8b123b7e6992039adfa5c15d8f63a6c495f7
Amirul <amirul.naim@zuscoffee.com>
2023-09-05 23:11:01 +0800

    Update shared data

--- resources/js/Layouts/Dashboard.vue
+++ resources/js/Layouts/Dashboard.vue
@@ -1,6 +1,10 @@
 <script setup>
-import { ref } from 'vue';
-import { Head, Link, router } from '@inertiajs/vue3';
+import { computed, ref } from 'vue';
+import { Head, Link, router, usePage } from '@inertiajs/vue3';
+
+const page = usePage()
+
+const user = computed(() => page.props.auth.user)
 
 defineProps({
     title: String,
@@ -40,10 +44,10 @@ const logout = () => {
                         <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
                             <div class="px-4 py-3" role="none">
                                 <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                    Neil Sims
+                                    {{ user.name }}
                                 </p>
                                 <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                    neil.sims@flowbite.com
+                                    {{ user.email }}
                                 </p>
                             </div>
                             <ul class="py-1" role="none">
