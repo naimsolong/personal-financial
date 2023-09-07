054c95841c32d3aa14aefbb03980edbf18f8c5c0
Amirul <amirul.naim@zuscoffee.com>
2023-09-07 17:44:27 +0800

    Add category relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
@@ -20,4 +21,12 @@ class Category extends Model
         'icon',
         'type',
     ];
+
+    /**
+     * The roles that belong to the categories.
+     */
+    public function group(): BelongsToMany
+    {
+        return $this->belongsToMany(CategoryGroup::class, 'category_pivot')->using(CategoryPivot::class);
+    }
 }
--- app/Models/CategoryGroup.php
+++ app/Models/CategoryGroup.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CategoryGroup extends Model
@@ -19,4 +20,12 @@ class CategoryGroup extends Model
         'name',
         'type',
     ];
+
+    /**
+     * The roles that belong to the categories.
+     */
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'category_pivot')->using(CategoryPivot::class);
+    }
 }
