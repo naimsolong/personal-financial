52923accbe8ea4ae9128c167acc6f60188e722ee
Amirul <amirul.naim@zuscoffee.com>
2023-09-07 17:44:15 +0800

    Create CategoryPivot model

---
+++ app/Models/CategoryPivot.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class CategoryPivot extends Pivot
+{
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_group_id',
+        'category_id',
+    ];
+}
