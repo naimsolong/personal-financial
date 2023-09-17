0e516afb54288ffa4cedf39efcfd858af03bb46f
Amirul <amirul.naim@zuscoffee.com>
2023-09-17 22:17:44 +0800

    Add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         if(app()->isLocal()) {
             $this->call([
                 CategorySeeder::class,
+                UserSeeder::class,
             ]);
         }
     }
