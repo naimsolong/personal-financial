f6460cccb88d7520da59a4b10ec48583bda4b0d0
Amirul <amirul.naim@zuscoffee.com>
2023-09-17 23:30:18 +0800

    Test currency

---
+++ tests/Unit/Enums/CurrencyTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use PrinsFrank\Standards\BackedEnum;
+use PrinsFrank\Standards\Currency\CurrencyAlpha3;
+
+test('have total 179 currency', function () {
+    $currency = BackedEnum::toArray(CurrencyAlpha3::class);
+    
+    expect($currency)->toBeArray()->toHaveCount(179);
+});
+
+test('able to get correct Euro currency', function () {
+    $currency = CurrencyAlpha3::from('EUR');
+
+    expect($currency->value)->toBe('EUR');
+    expect($currency->name)->toBe('Euro');
+    
+    expect($currency->toCurrencyNumeric()->value)->toBe('978');
+    expect($currency->toCurrencyName()->value)->toBe('Euro');
+    expect($currency->getSymbol()->value)->toBe('€');
+    expect($currency->lowerCaseValue())->toBe('eur');
+});
+    
+
+test('able to get correct Malaysia currency', function () {
+    $currency = CurrencyAlpha3::from('MYR');
+
+    expect($currency->value)->toBe('MYR');
+    expect($currency->name)->toBe('Malaysian_Ringgit');
+    
+    expect($currency->toCurrencyNumeric()->value)->toBe('458');
+    expect($currency->toCurrencyName()->value)->toBe('Malaysian Ringgit');
+    expect($currency->getSymbol()->value)->toBe('RM');
+    expect($currency->lowerCaseValue())->toBe('myr');
+});
