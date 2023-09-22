fb4348a6fae30497d81156c546172caa5155e729
Amirul <amirul.naim@zuscoffee.com>
2023-09-22 15:22:16 +0800

    Fix import class

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\TransactionsController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
