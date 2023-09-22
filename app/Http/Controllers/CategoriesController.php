199f330fbeacf6a7ce72c07dc65236548ba39761
Amirul <amirul.naim@zuscoffee.com>
2023-09-22 15:22:33 +0800

    Update index categories

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\TransactionsType;
+use App\Models\CategoryGroup;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CategoriesController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = CategoryGroup::select('id', 'name', 'type')->with('categories:id,name,type')->get();
+        
+        return Inertia::render('Dashboard/Categories/Index', [
+            'categories' => [
+                'expense' => $categories->where('type', TransactionsType::EXPENSE->value)->toArray(),
+                'income' => $categories->where('type', TransactionsType::INCOME->value)->toArray(),
+            ]
+        ]);
+    }
 }
