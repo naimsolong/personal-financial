f3ad573dea07a6495fca14f4773129926eb4f346
Amirul <amirul.naim@zuscoffee.com>
2023-09-22 15:22:00 +0800

    Add dashboard header

---
+++ resources/js/Components/Dashboards/Header.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <h1 class="text-lg font-bold">{{ props.title }}</h1>
+    <hr class="my-4">
+</template>
