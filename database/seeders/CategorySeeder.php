9d2d4cc3f63b353381f81839fe51842a26cc2864
Amirul <amirul.naim@zuscoffee.com>
2023-09-29 16:20:52 +0800

    Update category seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\TransactionsType;
 use App\Models\Category;
 use App\Models\CategoryGroup;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,8 +15,84 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        CategoryGroup::factory(rand(5,15))
-            ->has(Category::factory()->count(rand(1,10)))
-            ->create();
+        $group1 = CategoryGroup::create([
+            'name' => 'Bill',
+            'type' => TransactionsType::EXPENSE,
+        ]);
+        $group1->categories()->createMany([
+            [
+                'name' => 'Electricity',
+                'type' => TransactionsType::EXPENSE,
+            ],
+            [
+                'name' => 'Internet',
+                'type' => TransactionsType::EXPENSE,
+            ],
+            [
+                'name' => 'Water',
+                'type' => TransactionsType::EXPENSE,
+            ],
+        ]);
+
+        $group2 = CategoryGroup::create([
+            'name' => 'Food',
+            'type' => TransactionsType::EXPENSE,
+        ]);
+        $group2->categories()->createMany([
+            [
+                'name' => 'Breakfast',
+                'type' => TransactionsType::EXPENSE,
+            ],
+            [
+                'name' => 'Lunch',
+                'type' => TransactionsType::EXPENSE,
+            ],
+            [
+                'name' => 'Dinner',
+                'type' => TransactionsType::EXPENSE,
+            ],
+        ]);
+
+        $group3 = CategoryGroup::create([
+            'name' => 'Transportation',
+            'type' => TransactionsType::EXPENSE,
+        ]);
+        $group3->categories()->createMany([
+            [
+                'name' => 'Fuel',
+                'type' => TransactionsType::EXPENSE,
+            ],
+            [
+                'name' => 'Parking',
+                'type' => TransactionsType::EXPENSE,
+            ],
+            [
+                'name' => 'Toll',
+                'type' => TransactionsType::EXPENSE,
+            ],
+            [
+                'name' => 'Others',
+                'type' => TransactionsType::EXPENSE,
+            ],
+        ]);
+
+        $group4 = CategoryGroup::create([
+            'name' => 'Active',
+            'type' => TransactionsType::INCOME,
+        ]);
+        $group4->categories()->createMany([
+            [
+                'name' => 'Net Salary',
+                'type' => TransactionsType::INCOME,
+            ],
+            [
+                'name' => 'Freelance',
+                'type' => TransactionsType::INCOME,
+            ],
+        ]);
+
+        // CategoryGroup::factory(rand(5,15))
+        //     ->has(Category::factory()->count(rand(1,10)))
+        //     ->create();
     }
 }
