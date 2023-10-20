e453a0fd265f61c081f1d5b99f16e82b53a01343
Amirul <amirul.naim@zuscoffee.com>
2023-10-20 15:25:44 +0800

    rename a few flowbite

--- resources/js/Layouts/Dashboard.vue
+++ resources/js/Layouts/Dashboard.vue
@@ -29,10 +29,10 @@ const logout = () => {
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                         </svg>
                     </button>
-                    <a href="https://flowbite.com" class="flex ml-2 md:mr-24">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
-                        <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
-                    </a>
+                    <div class="flex ml-2 md:mr-24">
+                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Financial Logo" />
+                        <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Financial</span>
+                    </div>
                 </div>
                 <div class="flex items-center">
                     <div class="flex items-center ml-3">
@@ -198,7 +198,7 @@ const logout = () => {
                     </button>
                 </div>
                 <p class="mb-3 text-sm text-blue-800 dark:text-blue-400">
-                    Preview the new Flowbite dashboard navigation! You can turn the new navigation off for a limited time in your profile.
+                    Preview the new Financial dashboard navigation! You can turn the new navigation off for a limited time in your profile.
                 </p>
             </div>
         </div>
