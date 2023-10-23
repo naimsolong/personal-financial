f3dd2c156aae99c8fc28301621577598f92816d6
Amirul <amirul.naim@zuscoffee.com>
2023-10-23 21:11:23 +0800

    Update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
         UserSeeder::class,
         CategorySeeder::class,
         AccountSeeder::class,
+        WorkspaceSeeder::class,
         TransactionSeeder::class,
     ];
     
