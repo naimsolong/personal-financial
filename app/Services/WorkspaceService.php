ee7080bd0a315d96983126f75acc38fa306e0721
Amirul <amirul.naim@zuscoffee.com>
2023-10-27 15:04:04 +0800

    Add comment

--- app/Services/WorkspaceService.php
+++ app/Services/WorkspaceService.php
@@ -6,7 +6,14 @@ use App\Exceptions\ServiceException;
 use Illuminate\Support\Collection;
 
 class WorkspaceService extends BaseService
-{
+{    
+    /**
+     * store workspace function
+     *
+     * @param  mixed $model
+     * @param  mixed $data
+     * @return bool
+     */
     public function store(mixed $model = null, Collection $data): bool
     {
         $return = parent::store($model, $data);
@@ -15,7 +22,13 @@ class WorkspaceService extends BaseService
 
         return $return;
     }
-
+    
+    /**
+     * destroy workspace function
+     *
+     * @param  mixed $model
+     * @return bool
+     */
     public function destroy(mixed $model = null): bool
     {
         $this->setModel($model)->detachUser(auth()->user()->id);
@@ -24,17 +37,34 @@ class WorkspaceService extends BaseService
 
         return $return;
     }
-
+    
+    /**
+     * initiate workspace function to store current_workspace in session
+     *
+     * @return void
+     */
     public function initiate(): void
     {
         session()->put('current_workspace', request()->user()?->workspaces()->first()->id);
     }
-
+    
+    /**
+     * change workspace function to update current_workspace in session
+     *
+     * @param  mixed $workspace_id
+     * @return void
+     */
     public function change(int $workspace_id): void
     {
         session()->put('current_workspace', request()->user()?->workspaces()->find($workspace_id)->id);
     }
-
+    
+    /**
+     * attach user with current workspace
+     *
+     * @param  mixed $user_id
+     * @return void
+     */
     public function attachUser(int $user_id): void
     {
         $workspace = $this->getModel();
@@ -44,7 +74,13 @@ class WorkspaceService extends BaseService
 
         $workspace->users()->attach($user_id);
     }
-
+    
+    /**
+     * detach user from current workspace
+     *
+     * @param  mixed $user_id
+     * @return void
+     */
     public function detachUser(int $user_id): void
     {
         $workspace = $this->getModel();
