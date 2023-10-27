b317a32dd3fd29b99afe1bce647ee1526e415034
Amirul <amirul.naim@zuscoffee.com>
2023-10-27 16:30:07 +0800

    Update WorkspaceServiceTest

--- tests/Unit/Services/WorkspaceServiceTest.php
+++ tests/Unit/Services/WorkspaceServiceTest.php
@@ -4,7 +4,33 @@ use App\Models\User;
 use App\Models\Workspace;
 use App\Services\WorkspaceService;
 
-// TODO: Add more test
+it('able to store, update and destroy workspaces table', function() {
+    $user = User::factory()->create();
+    $service = app(WorkspaceService::class);
+    $this->actingAs($user);
+
+    $data = collect([
+        'name' => 'test'.rand(4,10),
+    ]);
+    $is_created = $service->store($data);
+    $this->assertDatabaseHas('workspaces', $data->toArray());
+    expect($is_created)->toBeTrue();
+    
+    $model = Workspace::factory()->create();
+    $data = collect([
+        'name' => 'test'.rand(4,10),
+    ]);
+    $is_updated = $service->update($model, $data);
+    $this->assertDatabaseHas('workspaces', collect($data)->merge([
+        'id' => $model->id
+    ])->toArray());
+    expect($is_updated)->toBeTrue();
+    
+    $model = Workspace::factory()->create();
+    $is_destroyed = $service->destroy($model);
+    $this->assertModelMissing($model);
+    expect($is_destroyed)->toBeTrue();
+});
 
 it('can initiate and change the current workspace', function () {
     $user = User::factory()->create();
@@ -26,4 +52,23 @@ it('can initiate and change the current workspace', function () {
     
     $response = $this->actingAs($user)->get('/');
     $response->assertSessionHas('current_workspace', $workspace->id);
+});
+
+it('can attach and detach from the current workspace', function () {
+    $user = User::factory()->create();
+    $workspace = Workspace::factory()->create();
+    $service = app(WorkspaceService::class);
+    $service->setModel($workspace);
+    
+    $service->attachUser($user->id);
+    $this->assertDatabaseHas('workspace_users', [
+        'workspace_id' => $workspace->id,
+        'user_id' => $user->id,
+    ]);
+
+    $service->detachUser($user->id);
+    $this->assertDatabaseMissing('workspace_users', [
+        'workspace_id' => $workspace->id,
+        'user_id' => $user->id,
+    ]);
 });
