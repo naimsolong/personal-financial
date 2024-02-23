34655727be1eda7a09d7e1d85e04defc048449b8
naimsolong <naimsolong@users.noreply.github.com>
2024-02-23 07:11:23 +0000

    Fix styling

--- tests/Feature/Admin/AdminTest.php
+++ tests/Feature/Admin/AdminTest.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Testing\AssertableInertia as Assert;
 
 test('admin able access admin page', function (User $user) {
