3fa1f341e15ff4413d99e72ccc92eb8487508bcf
Amirul <amirul.naim@zuscoffee.com>
2024-02-23 15:32:45 +0800

    Prepare admin waitlist page

---
+++ resources/js/Pages/Admin/Waitlists/Index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Layout from '@/Layouts/Admin.vue';
+</script>
+
+<template>
+    <Layout title="Waitlist">
+        Waitlist
+    </Layout>
+</template>
--- routes/admins/index.php
+++ routes/admins/index.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\Admin\WaitlistController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/admin')->name('admin')->group(function() {
     Route::get('/', IndexController::class)->name('.index');
+
+    Route::prefix('/waitlist')->name('.waitlist')->group(function() {
+        Route::get('/', [WaitlistController::class, 'index'])->name('.index');
+    });
 });
