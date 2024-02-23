28a889987a4930b85fc56726822011b67a72b68f
naimsolong <naimsolong@users.noreply.github.com>
2024-02-23 07:33:12 +0000

    Fix styling

--- routes/admins/index.php
+++ routes/admins/index.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\Admin\IndexController;
 use App\Http\Controllers\Admin\WaitlistController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/admin')->name('admin')->group(function() {
+Route::prefix('/admin')->name('admin')->group(function () {
     Route::get('/', IndexController::class)->name('.index');
 
-    Route::prefix('/waitlist')->name('.waitlist')->group(function() {
+    Route::prefix('/waitlist')->name('.waitlist')->group(function () {
         Route::get('/', [WaitlistController::class, 'index'])->name('.index');
     });
 });
