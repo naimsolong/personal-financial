2681de4cb86719017401df718d51be295e14f0a8
Amirul <amirul.naim@zuscoffee.com>
2024-02-23 11:30:42 +0800

    Update test setup

--- _volumes/scripts/01-init.sql
+++ _volumes/scripts/01-init.sql
@@ -1,3 +1,6 @@
 -- create databases
 CREATE DATABASE IF NOT EXISTS `personal_financial`;
 CREATE DATABASE IF NOT EXISTS `personal_financial_test`;
+
+-- grant user rights
+GRANT ALL ON *.* TO 'dbuser'@'%';
